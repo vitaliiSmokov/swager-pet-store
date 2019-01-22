ac1ad8ce12e72581ff04cd0999c12e900d706742
vitalii_smokov <vitalik23@ukr.net>
2019-01-22 08:06:58 +0200

    Added request specification

--- src/main/java/io/swagger/petstore/controller/pet/PetCtrl.java
+++ src/main/java/io/swagger/petstore/controller/pet/PetCtrl.java
@@ -1,21 +1,29 @@
 package io.swagger.petstore.controller.pet;
 
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
 import io.swagger.petstore.controller.AbstractController;
 import io.swagger.petstore.model.pet.PetModel;
+
+import java.util.HashMap;
 
 import static io.restassured.RestAssured.given;
 
 public class PetCtrl extends AbstractController {
 
+    private RequestSpecBuilder petSpec = new RequestSpecBuilder()
+            .addCookie("coockie")
+            .addHeaders(new HashMap<>());
+
+
     public PetCtrl() {
 //        RestAssured.requestSpecification.basePath("/v2/pet");
-//        RestAssured.requestSpecification = new RequestSpecBuilder().setBasePath("/v2/pet").build();
         RestAssured.basePath = "/v2/pet";
     }
 
     public PetModel addNewPet(PetModel pet) {
-        return given().body(pet)
+        return given(petSpec.build())
+                .body(pet)
                 .when().post()
                 .as(PetModel.class);
     }
